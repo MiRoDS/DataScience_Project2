d1774879ca7dc76c0b4b740d3afdb815182bb7b3
MiRoDS <59603686+MiRoDS@users.noreply.github.com>
2020-02-09 22:03:55 +0100

    Process data script runs

--- data/process_data.py
+++ data/process_data.py
@@ -1,18 +1,62 @@
+# import libraries
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # Read in messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # Read in categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # Merge messages and categories datasets    
+    df=messages.merge(categories, on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=";", expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.map(lambda i: (i[:-2]))
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].map(lambda i: (i[-1:]))
+    
+    # drop the original categories column from `df`
+    del df['categories']
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    # Create a database
+    engine = create_engine(database_filename)
+    
+    #Store the dataframe as a database table
+    df.to_sql('OneAndOnlyTable', engine, index=False)  
 
-
+    
 def main():
     if len(sys.argv) == 4:
 
@@ -21,10 +65,10 @@ def main():
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
-
+        
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
         
