bab3a90c9bb953ba70713beba9bc1c98cf181cc5
MiRoDS <59603686+MiRoDS@users.noreply.github.com>
2020-02-09 22:04:36 +0100

    Add files via upload

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,13 +1,55 @@
 import sys
 
+# download necessary NLTK data
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+# import libraries
+from sqlalchemy import create_engine
+
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+import pickle
+
 
 def load_data(database_filepath):
-    pass
+	# load data from database
+	engine = create_engine(database_filepath)
+	df = pd.read_sql_table('OneAndOnlyTable', engine)
+	X = df['message']
+	Y = df.loc[:, 'related':'direct_report']
+	category_names = pd.Series(df.columns)
+	category_names = category_names.loc[4:]
+	return X, Y, category_names
 
-
+	
 def tokenize(text):
-    pass
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+	
 
 def build_model():
     pass
