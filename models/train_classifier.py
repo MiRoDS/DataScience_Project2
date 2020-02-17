ad8471d6f3035c8bc6fbbac3e5e403d6dbe7eef2
MiRoDS <59603686+MiRoDS@users.noreply.github.com>
2020-02-17 23:00:50 +0100

    Docstrings added + missing import added

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -15,6 +15,7 @@ from nltk.stem import WordNetLemmatizer
 
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -29,18 +30,46 @@ import pickle
 
 # Loads data prepared by an ETL pipeline from a SQLite database
 def load_data(database_filepath):
+    '''
+    INPUT:
+    database_filepath - Path to the SQLite database
+
+    OUTPUT:
+    X - Dataframe which contains the messages
+    Y - Dataframe which Contains the categories
+    category_names - Names of the categories
+
+    This function loads data prepared by an ETL pipeline from a SQLite database.
+    '''
+    
     # load data from database
     engine = create_engine("sqlite:///"+database_filepath)
     df = pd.read_sql_table('OneAndOnlyTable', engine)
+    
+    # Get the messages
     X = df['message']
+    
+    # Get the categories
     Y = df.loc[:, 'related':'direct_report']
-    category_names = pd.Series(df.columns)
-    category_names = category_names.loc[4:]
+    
+    # Get the category names
+    category_names = pd.Series(df.columns).loc[4:].values
+    
     return X, Y, category_names
 
 
 # Tokenizes text
 def tokenize(text):
+    '''
+    INPUT:
+    text - Input text to be tokenized
+
+    OUTPUT:
+    clean_tokens - Output of tokens after word tokenization, use of lower case, and lemmatization
+
+    This function tokenizes text.
+    '''
+    
     text = re.sub(r"[^a-zA-Z0-9]", " ", text)
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
@@ -55,6 +84,16 @@ def tokenize(text):
 
 # Builds a model
 def build_model():
+    '''
+    INPUT:
+    n/a
+
+    OUTPUT:
+    model - Output of the build GridSearchCV model
+
+    This function builds a model by using a pipeline with CountVectorizer, TfidfTransformer, and a multi-output RandomForestClassifier. GridSearch is used for parameter optimization.
+    '''
+    
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
@@ -73,6 +112,19 @@ def build_model():
 
 # Evaluates the model
 def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    INPUT:
+    model - The GridSearchCV model to be evaluated
+    X_test - Test data containing the messages
+    Y_test - Test data containing the categories
+    category_names - Names of the categories
+
+    OUTPUT:
+    n/a
+
+    This function evaluates the previously created model and creates console output of different statistics.
+    '''
+    
     # Predict categories for test data
     Y_pred = model.predict(X_test)
     
@@ -86,11 +138,22 @@ def evaluate_model(model, X_test, Y_test, category_names):
     for cat in range(len(category_names)):
         print("Category:", category_names[cat]) # Print the category name
         print("Accuracy:", (Y_test.loc[:, category_names[cat]] == Y_pred[:, cat]).mean()) # Print the category accuracy
-        print(classification_report(Y_test.loc[:, category_names[cat]], Y_pred[:, cat])) # Print the corresponding classification report
+        print(classification_report(Y_test.loc[:, category_names[cat]].values, Y_pred[:, cat], digits=3)) # Print the corresponding classification report
     
 
 # Saves model as pickle file
 def save_model(model, model_filepath):
+    '''
+    INPUT:
+    model - The GridSearchCV model to be evaluated
+    model_filepath - Path for the pickle file to be saved
+
+    OUTPUT:
+    n/a
+
+    This function saves the model as a pickle file.
+    '''
+    
     with open(model_filepath, 'wb') as f: 
         pickle.dump(model, f)
 
